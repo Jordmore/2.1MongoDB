bb2bbf86ca17e1981868bfb428b57dd0b7834b58
JORDON SEYMOUR <V8027914@live.tees.ac.uk>
2020-11-27 12:22:27 +0000

    Add project files.

---
+++ 2.1MongoDB/Context/OrdersContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _2._1MongoDB.Context
+{
+    public class OrdersContext: IOrdersContext
+        private readonly IMongoDatabase _db;
+    {
+
+}
+}
